e2c5213930ca5c2e767c4f3e9fc5c855e62fe5b7
shironeko <shironeko1052@gmail.com>
2024-05-09 10:13:17 -0700

    fix bug, update README

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('keydown', (e) => {
+document.addEventListener('keydown', async (e) => {
   const activeEle = document.activeElement;
   const videoEl = document.querySelector('ytd-player:not(ytd-video-preview ytd-player) video');
   const videoContainerEl = document.querySelector('#container');
@@ -15,11 +15,12 @@ document.addEventListener('keydown', (e) => {
     if (e.code === 'Space') {
       // compare the state, if no change then toggles play state
       const curState = videoEl.paused;
+      const { delay } = (await chrome.storage.local.get('delay')) || { delay: 200 };
       setTimeout(() => {
         if (videoEl.paused === curState) {
           playButton.click();
         }
-      }, chrome.storage.local.get('delay') || 200);
+      }, delay);
     }
   }
 })
